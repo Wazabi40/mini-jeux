6e624f3b7280a90b1c15d4b853655e1539405ce2
gael vtl <gael.vtl@outlook.fr>
2022-02-12 17:23:54 +0100

    css page_creation_compte

--- page_creation_compte.php
+++ page_creation_compte.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+    <link rel = "stylesheet" href = "./css/style.css"/>
         <title>Mini jeux</title>
         <meta charset = "UTF-8">
         <name="viewport" content="width=device.width, initial-scale=1.0">
@@ -9,26 +10,29 @@
 
 
 <body>
-    <h2> Creation de votre compte: </h2>    
-    <form action = "fonction_creationCompte.php" method = "post">
-        <label> Nom Prenom: </label></br>
-            <input type = "text"  name = "nom"  placeholder = "NOM Prenom" /></br></br>
-        <label> Adresse Mail: </label></br>
-            <input type = "text"  name = "mail" placeholder = "Adresse Mail" /><br/></br>
-        <label> Mot de passe: </label></br>
-            <input type = "passeword" name = "pwd" placeholder = "Mot de passe" /> <br/></br>
-        <label> Identidiant: </label></br>
-            <input type = "text"  name = "login"  placeholder = "Identifiant" /></br></br>
+    <br>
+    <h1> Minis-Jeux </h1>
+    <div id = "formulaireConnexion">
+        <h2> Creation de votre compte: </h2>    
+        <form action = "fonction_creationCompte.php" method = "post">
+            <label> Nom Prenom: </label></br>
+                <input type = "text"  name = "nom"  placeholder = "NOM Prenom" /></br></br>
+            <label> Adresse Mail: </label></br>
+                <input type = "text"  name = "mail" placeholder = "Adresse Mail" /><br/></br>
+            <label> Mot de passe: </label></br>
+                <input type = "passeword" name = "pwd" placeholder = "Mot de passe" /> <br/></br>
+            <label> Identidiant: </label></br>
+                <input type = "text"  name = "login"  placeholder = "Identifiant" /></br></br>
 
-        <input type = "submit"  value = "creer compte" /></br></br>
-        <label>
-            <small>
-                vous avez déjà un compte ? <a href = "page_connexion.php" >connectez-vous ! </a>
-            </small>
+            <input type = "submit"  value = "creer compte" /></br></br>
+            <label>
+                <small>
+                    vous avez déjà un compte ? <a href = "page_connexion.php" >connectez-vous ! </a>
+                </small>
 
-        </label>
-        
-        
+            </label>
+        </form>
+    </div>   
 
             
 
